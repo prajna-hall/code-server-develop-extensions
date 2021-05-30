139beaf781cd6e51f0fd4b0c93546dfaffe70f70
humbinal <humbinal@126.com>
2021-05-30 16:36:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -36,8 +36,12 @@ sleep 3
 # 仅仅启动也没用,需要模拟一次访问,此时code-server才会在user-data-dir中生成运行时文件,后续安装语言包才能生成languagepacks.json文件
 curl http://localhost:8081 > /dev/null
 
-# native-ascii-converter插件直接通过名称安装出现找不到的错误,因此手动下载,通过指定vsix安装
+# 以下插件直接通过名称安装出现找不到的错误,因此手动下载,通过指定vsix安装
 curl -J -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/cwan/vsextensions/native-ascii-converter/1.0.9/vspackage | gunzip > /tmp/native-ascii-converter-1.0.9.vsix
+curl -J -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/adpyke/vsextensions/vscode-sql-formatter/1.4.4/vspackage | gunzip > /tmp/adpyke.vscode-sql-formatter-1.4.4.vsix
+curl -J -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/mtxr/vsextensions/sqltools/0.23.0/vspackage | gunzip > /tmp/mtxr.sqltools-0.23.0.vsix
+curl -J -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/mtxr/vsextensions/sqltools-driver-pg/0.2.0/vspackage | gunzip > /tmp/mtxr.sqltools-driver-pg-0.2.0.vsix
+curl -J -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/zaaack/vsextensions/markdown-editor/0.1.7/vspackage | gunzip > /tmp/zaaack.markdown-editor-0.1.7.vsix
 
 /usr/local/code-server/bin/code-server \
   --extensions-dir /usr/local/code-server-extensions \
@@ -76,8 +80,11 @@ curl -J -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/
   --install-extension dbaeumer.vscode-eslint \
   --install-extension ms-azuretools.vscode-docker \
   --install-extension samuelcolvin.jinjahtml \
-  --install-extension cymonk.sql-formatter \
-  --install-extension /tmp/native-ascii-converter-1.0.9.vsix
+  --install-extension /tmp/native-ascii-converter-1.0.9.vsix \
+  --install-extension /tmp/adpyke.vscode-sql-formatter-1.4.4.vsix \
+  --install-extension /tmp/mtxr.sqltools-0.23.0.vsix \
+  --install-extension /tmp/mtxr.sqltools-driver-pg-0.2.0.vsix \
+  --install-extension /tmp/zaaack.markdown-editor-0.1.7.vsix
 
 echo "INFO: all extensions install finished."
 
